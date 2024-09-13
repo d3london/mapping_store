09d499e2d701d93dc370bc3c8cac85aa859bbe15
Lawrence Adams <lawrencetadams@googlemail.com>
2024-09-13 19:15:25 +0100

    Use partial index on live mappings

--- migrations/20240907112915_Initialise_mapping_store.sql
+++ migrations/20240907112915_Initialise_mapping_store.sql
@@ -46,7 +46,10 @@ CREATE TABLE mappings.concept_relationship
     relationship_id  varchar(20)                     NOT NULL,
     valid_start_date date DEFAULT NOW()              NOT NULL,
     valid_end_date   date DEFAULT date('2099-12-31') NOT NULL,
-    invalid_reason   varchar(1),
-    CONSTRAINT unique_live_mapping_constraint
-        UNIQUE (concept_id_1, concept_id_2, valid_end_date)
+    invalid_reason   varchar(1)
 );
+
+-- Use partial index so only the 'live' mappings are checked.
+CREATE UNIQUE INDEX unique_live_mapping_constraint
+    ON mappings.concept_relationship (concept_id_1, concept_id_2, relationship_id)
+    WHERE invalid_reason IS NULL;
